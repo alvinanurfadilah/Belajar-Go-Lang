55ea90a22ab779bd153f5fde9592b84f5e16b457
alvinanurfadilah <alvinanurfadilah@gmail.com>
2023-09-15 18:32:48 +0700

    Type declarations

---
+++ type-declarations.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	type NIK string
+// 	type Married bool
+
+// 	var NIKVina NIK = "1234567890987654321"
+// 	var marriedStatus Married = false
+
+// 	fmt.Println(NIKVina)
+// 	fmt.Println(marriedStatus)
+// }
