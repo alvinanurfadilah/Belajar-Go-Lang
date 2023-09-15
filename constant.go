5b5719f7c7939fd179e01508bbfb790015656c3e
alvinanurfadilah <alvinanurfadilah@gmail.com>
2023-09-15 18:02:59 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,27 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	// const firstName string = "Alvina"
+// 	// const lastName = "Nurfadilah"
+// 	// const value = 1000
+
+// 	/* constant bisa dibuat multiple, contohnya seperti dibawah */
+
+// 	const (
+// 		firstName string = "Alvina"
+// 		lastName         = "Nurfadilah"
+// 		value            = 1000
+// 	)
+
+// 	//fmt.Println(firstName)
+// 	fmt.Println(lastName)
+// 	fmt.Println(value)
+
+// 	// error
+// 	/* firstName = "Tidak bisa diubah"
+// 	lastName = "Tidak bisa diubah" */
+
+// 	/* tidak seperti variabel, saat membuat constant tetapi tidak digunakan, tidak akan error */
+// }
