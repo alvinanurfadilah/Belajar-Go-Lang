68cdfa2d95d8f364413f0914e9b302fa3ae8ddb3
alvinanurfadilah <alvinanurfadilah@gmail.com>
2023-09-19 11:02:01 +0700

    Array

---
+++ array.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var names [3]string
+
+// 	names[0] = "Alvina"
+// 	names[1] = "Nurfadilah"
+
+// 	fmt.Println(names[0])
+// 	fmt.Println(names[1])
+
+// 	var values = [3]int{
+// 		90,
+// 		85,
+// 		80,
+// 	}
+
+// 	fmt.Println(values)
+// 	fmt.Println(values[0])
+// 	fmt.Println(values[1])
+// 	fmt.Println(values[2])
+
+// 	// untuk mengetahui jumlah panjang array
+// 	fmt.Println(len(names))
+// 	fmt.Println(len(values))
+// }
